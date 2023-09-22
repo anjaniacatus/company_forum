a50f4c80ae7b56e8ec1392a1bd4de725d3a5c7d8
aina_fenoum <anja1catus@gmail.com>
2023-09-22 09:40:12 +0300

    Désactiver les thèmes par défaut

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/templates/**/*.{html,js}"],
+
   theme: {
     extend: {},
   },
   plugins: [require("daisyui"), require('@tailwindcss/forms')],
+  daisyui: {
+      themes: [],
+  },
 }
 
