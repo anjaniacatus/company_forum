b983d0b9637d73c81c6917bcfbe708dd0758f686
aina_fenoum <anja1catus@gmail.com>
2023-09-24 15:46:14 +0300

    créer/modifier une question

--- src/faq/views.py
+++ src/faq/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from .models import Question
+from .forms import QuestionForm
 
 
 def question_list(request):
@@ -14,3 +15,37 @@ def question_list(request):
         "../templates/pages/question_list.html",
         {"questions_per_page": questions_per_page},
     )
+
+
+def question_new(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+            return redirect("question_detail", pk=question.pk)
+    else:
+        form = QuestionForm()
+        return render(request, "../templates/faq/question_edit.html", {"form": form})
+
+
+def question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(
+        request, "../templates/faq/question_detail.html", {"question": question}
+    )
+
+
+def question_edit(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+            return redirect("question_detail", pk=question.pk)
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, "../templates/faq/question_edit.html", {"form": form})
